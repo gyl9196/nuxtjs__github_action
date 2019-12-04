f36d8b3354999d4dfacc8524ac44df69cec395ec
Yanlin Gong <yanlin.gong@shroogle.com.au>
2019-12-04 16:17:11 +1100

    chore: init

---
+++ index.js
@@ -0,0 +1,16 @@
+// const a = 2
+// const b = 3
+
+// const result = 'hello'
+// switch (result) {
+//     case 'hello':
+//         console.log('hello');
+//         break;
+//     case 'hello':
+//         console.log('again hello');
+//         break;
+//     default:
+//         console.log('default')
+// }
+
+console.log(JSON.parse('----------------------------031405862445975522890030\r\nContent-Disposition: form-data; name="username"\r\n\r\nniel\r\n----------------------------031405862445975522890030\r\nContent-Disposition: form-data; name="password"\r\n\r\n123456\r\n----------------------------031405862445975522890030--\r\n'))
